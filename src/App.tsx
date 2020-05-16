0206db1b504d0d1883e565f88fe38700a4cffb57
Gerardo Paredes <gerardparedes23@gmail.com>
2020-05-16 15:32:59 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { 
+  IonApp, 
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonLabel,
+  IonInput } from '@ionic/react';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>BMI Calculator</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonLabel></IonLabel>
+            <IonInput></IonInput>
+          </IonCol>
+          <IonCol>
+            <IonLabel></IonLabel>
+            <IonInput></IonInput>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </IonContent>
+  </IonApp>
+);
+
+export default App;
